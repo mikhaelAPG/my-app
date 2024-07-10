903aa569bb263232b120011faaff7713f88cf0b3
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-11 02:36:36 +0700

    first commit

---
+++ application/views/percobaan.php
@@ -0,0 +1,5 @@
+<h1>Ini halaman percobaan</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eos nulla inventore placeat libero, ex porro labore tempora quia quo vero esse minus enim dolores ratione dolore ut rerum. Quasi?</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, nisi. Vel explicabo iste, distinctio fuga perspiciatis velit, doloremque officia totam reprehenderit ut eaque aut, voluptates qui rerum molestias beatae incidunt.</p>
