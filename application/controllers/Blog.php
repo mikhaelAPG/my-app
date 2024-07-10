f2ecf6a5ff0e37f98bee249ac81a8264ac6770b5
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-11 02:50:29 +0700

    view

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -3,8 +3,10 @@
 class Blog extends CI_Controller {
     public function index($nama, $golDarah, $alamat)
     {
-        echo "Selamat Datang $nama <br>";
-        echo "Golongan Darah Anda $golDarah <br>";
-        echo "Anda tinggal di alamat $alamat <br>";
+       $data['nama'] = $nama;
+       $data['golDarah'] = $golDarah;
+       $data['alamat'] = $alamat;
+       
+       $this->load->view("blog", $data);
     }
 }
---
+++ application/views/blog.php
@@ -0,0 +1,3 @@
+<h1>Selamat Datang <?php echo $nama; ?></h1> <br>
+Golongan Darah Anda <?php echo $golDarah; ?> <br>
+Anda tinggal di alamat <?php echo $alamat; ?> <br>
