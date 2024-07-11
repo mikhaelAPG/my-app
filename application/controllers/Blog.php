524b4f0ba0eb4fdfabc6e7b3a6baea82a5982a45
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-11 11:13:15 +0700

    akses database

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -3,19 +3,9 @@
 class Blog extends CI_Controller {
     public function index()
 	{
-		$data['blogs']=[
-			['title'=>'Artikel Pertama',
-            'content'=>'<p>Lorem Ipsum is simply dummy text of the printing and 	
-             typesetting industry.</p>'],
-            ['title'=>'Artikel Kedua',
-            'content'=>'<p>Lorem Ipsum has been the industrys standard dummy 
-             text ever since the 1500s, when an unknown printer took a galley of type 
-             and scrambled it to make a type specimen book.</p>'],
-            ['title'=>'Artikel Ketiga',
-            'content'=>'<p>Lorem Ipsum is simply dummy text of the printing and 	
-             typesetting industry. Lorem Ipsum has been the industrys standard dummy 
-             text ever since the 1500s.</p>']
-            ];
+        $this->load->database();
+		$query=$this->db->query("select * from blog");
+		$data['blogs']=$query->result_array();
         $this->load->view('blog',$data);
 	}
 }
