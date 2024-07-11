ec6a916cf1ca412b27df2a137a0ae42a960d09df
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-11 11:40:08 +0700

    Select Where

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,11 +1,27 @@
 <?php
 
 class Blog extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+        $this->load->helper('url');
+    }
+
     public function index()
 	{
-        $this->load->database();
-		$query=$this->db->query("select * from blog");
-		$data['blogs']=$query->result_array();
-        $this->load->view('blog',$data);
+        $query = $this->db->get("blog");
+        $data['blogs'] = $query->result_array(); 
+        
+        $this->load->view('blog', $data);
 	}
+
+    public function detail($url)
+    {
+        $query = $this->db->query('SELECT * FROM blog WHERE url = "'.$url.'"');
+        $data['post'] = $query->row_array();
+
+        $this->load->view('detail', $data);
+    }
 }
---
+++ application/views/detail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $post['title']; ?></title>
+</head>
+<body>
+  <h1><?php echo $post['title']; ?></h1>
+  <?php echo $post['content']; ?>
+</body>
+</html>
